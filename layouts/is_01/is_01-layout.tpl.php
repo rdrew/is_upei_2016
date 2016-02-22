86bab75cde56cb58ff95636c7e349aae738bf488
rdrew <rdrew@upei.ca>
2016-02-22 19:24:07 -0400

    scholar template

--- layouts/is_01/is_01-layout.tpl.php
+++ layouts/is_01/is_01-layout.tpl.php
@@ -9,12 +9,14 @@
 		<?php if ($logo): ?>
 		  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
 		<?php endif; ?>
-		<?php if ($site_name): ?>
-		  <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-		<?php endif; ?>
-		<?php if ($site_slogan): ?>
-		  <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
-		<?php endif; ?>
+		<div class="name_slogan">
+			<?php if ($site_name): ?>
+			  <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+			<?php endif; ?>
+			<?php if ($site_slogan): ?>
+			  <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
+			<?php endif; ?>
+		</div>
 		<?php print render($page['branding']); ?>
 	  </div>
 	  <?php print render($page['header']); ?>
