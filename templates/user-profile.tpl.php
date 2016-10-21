bb81301e46e475a603660aa6902679e4830b7d6c
rdrew <rdrew@upei.ca>
2016-10-21 16:24:15 -0300

    .

--- templates/user-profile.tpl.php
+++ templates/user-profile.tpl.php
@@ -9,65 +9,74 @@
 drupal_add_library ( 'system' , 'ui.tabs' );
 drupal_add_js ( 'jQuery(document).ready(function(){
 	jQuery("#tabs").tabs();
+	var identifier = jQuery("#identifier").text();
+	var url = "/people/" + identifier + " " + ".l-content";
+	jQuery("#tabs-1").load(url);
+	
    });
    ' , 'inline' );
    ?>
 
+   <span id="identifier" style="display: none;" ><?php print render($identifier); ?></span>
 <article<?php print $attributes; ?>>
    <?php print render($user_profile); ?>
    <hr>
    <h3>
-      Your Scholar Profile
+      Manage Your Scholar Profile
    </h3>
    <div class="block--profile_admin">
-      <!--<button class="green">-->
-      <!--<i class="fa fa-user" aria-hidden="true"></i>-->
-      <!--<a style="color: #fff;" href="/people/<?php print $identifier; ?>">-->
-      <!--View your profile-->
-      <!--</a>-->
-      <!--</button>-->
-      <!--<button class="green">-->
-      <!--<i class="fa fa-gear" aria-hidden="true"></i>-->
-      <!--<a style="color: #fff;" href="/update-publications">-->
-      <!--Edit your profile-->
-      <!--</a>-->
-      <!--</button>-->
    </div>
-</article>
 <div id="tabs">
    <ul>
-      <li><a href="#tabs-1">Tab 1</a></li>
-      <li><a href="#tabs-2">Tab 2</a></li>
-      <li><a href="#tabs-3">Tab 3</a></li>
+	  <li><a href="#tabs-1"><i class="fa fa-eye" aria-hidden="true"></i>&nbsp;Preview </a></li>
+	  <li><a href="#tabs-2"><i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit</a></li>
+	  <li><a href="#tabs-3"><i class="fa fa-list" aria-hidden="true"></i>&nbsp;List Publications</a></li>
+	  <li><a href="#tabs-4"><i class="fa fa-plus" aria-hidden="true"></i>&nbsp;Add Publication(s)</a></li>
    </ul>
+<!--
+   -tab 1
+   -->
+
    <div id="tabs-1">
-      <div class="view--citations">
-         <h3>
-            Your Recent Publications
-         </h3>
-         <!--<button class="green">-->
-         <!--<i class="fa fa-gear" aria-hidden="true"></i>-->
-         <!--<a style="color: #fff;" href="/update-publications">-->
-         <!--Submit new publications-->
-         <!--</a>-->
-         <!--</button>-->
-         <?php print views_embed_view('recent_citations', 'user_block', $identifier); ?>
-      </div>
    </div>
+<!--
+   -tab 2
+   -->
    <div id="tabs-2">
-      <h2>
-         Submit New Publications
-      </h2>
-      <?php
-         $block = module_invoke('entityform_block', 'block_view', 'add_publications');
-         print render($block['content']);
-         //$block = module_invoke('webform', 'block_view', 'client-block-15');
-         //print render($block['content']);
-         ?>
    </div>
+<!--
+   -tab 3
+   -->
    <div id="tabs-3">
-      <h2>
-         test
-      </h2>
+	  <div class="view--citations">
+		 <h3>
+			Your Recent Publications
+		 </h3>
+		 <!--<button class="green">-->
+		 <!--<i class="fa fa-gear" aria-hidden="true"></i>-->
+		 <!--<a style="color: #fff;" href="/update-publications">-->
+		 <!--Submit new publications-->
+		 <!--</a>-->
+		 <!--</button>-->
+		 <?php print views_embed_view('recent_citations', 'user_block', $identifier); ?>
+	  </div>
    </div>
+<!--
+   -tab 4
+   -->
+   <div id="tabs-4">
+	  
+	  <h2>
+		 Submit New Publications
+	  </h2>
+<?php
+   $block = module_invoke('entityform_block', 'block_view', 'add_publications');
+   print render($block['content']);
+?>
+
+   </div>
+<!--
+   -end of tabs
+   -->
 </div>
+</article>
